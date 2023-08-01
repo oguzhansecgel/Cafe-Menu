fc4794b1e9e4640f774517ef55b9d85574a47850
oguzhansecgel <oguzhansecgel19@icloud.com>
2023-08-02 01:04:29 +0300

    ProductCategory list tekrar eklendi.

--- 2-FrontEnd/MyPortfolio.UI/Controllers/AdminPaneli/ProductController.cs
+++ 2-FrontEnd/MyPortfolio.UI/Controllers/AdminPaneli/ProductController.cs
@@ -13,90 +13,97 @@ namespace MyPortfolio.UI.Controllers.AdminPaneli
 {
 	public class ProductController : Controller
 	{
-		Context c = new Context();
-		private readonly IHttpClientFactory _httpClientFactory;
+        Context c = new Context();
+        private readonly IHttpClientFactory _httpClientFactory;
 
-		public ProductController(IHttpClientFactory httpClientFactory)
-		{
-			_httpClientFactory = httpClientFactory;
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
 
-		}
+        }
 
-		public async Task<IActionResult> Index() // listeleme metodu
-		{
-			var client = _httpClientFactory.CreateClient();
-			var responserMessage = await client.GetAsync("http://localhost:5185/api/Product");
-			if (responserMessage.IsSuccessStatusCode)
-			{
-				var jsonData = await responserMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
-				return View(values);
-			}
-			return View();
-		}
-		public async Task<IActionResult> DeleteProduct(int id)
-		{
-			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.DeleteAsync($"http://localhost:5185/api/Product/{id}");
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				return RedirectToAction("Index");
-			}
-			return View();
-
-
-		}
+        public async Task<IActionResult> Index() // listeleme metodu
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responserMessage = await client.GetAsync("http://localhost:5185/api/Product");
+            if (responserMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responserMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:5185/api/Product/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
 
 
-		[HttpGet]
-		public IActionResult AddProduct()
-		{
-			return View();
-		}
-		[HttpPost]
-		public async Task<IActionResult> AddProduct(AddProductDto model)
-		{
-
-			var client = _httpClientFactory.CreateClient();
-			var jsonData = JsonConvert.SerializeObject(model);
-			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-			var responseMessage = await client.PostAsync("http://localhost:5185/api/Product", stringContent);
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				return RedirectToAction("Index");
-			}
-			return View();
-
-		}
-
-		[HttpGet]
-		public async Task<IActionResult> UpdateProduct(int id)
-		{
-			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync($"http://localhost:5185/api/Product/{id}");
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<UpdateProductDto>(jsonData);
-				return View(values);
-			}
-			return View();
-		}
-
-		[HttpPost]
-		public async Task<IActionResult> UpdateProduct(UpdateProductDto model)
-		{
-			var client = _httpClientFactory.CreateClient();
-			var jsonData = JsonConvert.SerializeObject(model);
-			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-			var responseMessage = await client.PutAsync($"http://localhost:5185/api/Product/", stringContent);
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				return RedirectToAction("Index");
-			}
-			return View();
-		}
+        }
 
 
-	}
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            List<SelectListItem> values = (from x in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }
+                                           ).ToList();
+            ViewBag.v1 = values;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(AddProductDto model)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5185/api/Product", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5185/api/Product/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync($"http://localhost:5185/api/Product/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
 }
